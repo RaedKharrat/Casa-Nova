4737f42f507f59b074f14567fefedcc74bcbe589
RaedKharrat <kharrat.raed@esprit.tn>
2026-04-11 12:01:48 +0100

    fix: remove unused useEffect in Loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 import casanovaLogo from '../../assets/casanova.png';
